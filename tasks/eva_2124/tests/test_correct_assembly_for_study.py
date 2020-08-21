11b35ee2dd49935ec309ae0e0e2583d84dd6037f
jose miguel mut <jomutlo@gmail.com>
2020-08-21 16:43:58 +0100

    updated tests

--- tasks/eva_2124/tests/test_correct_assembly_for_study.py
+++ tasks/eva_2124/tests/test_correct_assembly_for_study.py
@@ -4,7 +4,7 @@ from unittest import TestCase
 import pymongo
 from ebi_eva_common_pyutils.mongo_utils import get_mongo_connection_handle
 
-from tasks.eva_2124.correct_assembly_for_study import correct
+from tasks.eva_2124.correct_assembly_for_study import correct, get_mongo_connection_handle_url
 
 
 class TestCorrectAssembly(TestCase):
@@ -13,17 +13,13 @@ class TestCorrectAssembly(TestCase):
         # setup mongo database
         self.host = 'localhost'
         self.port = 27017
-        self.admin_db = 'admin'
-        self.username = 'admin'
-        self.password = 'admin'
         self.eva_database = 'eva_accession_sharded'
         self.variant_collection = 'submittedVariantEntity'
-        mongo_connection_uri = "mongodb://{0}:{1}/{2}".format(self.host, self.port, self.admin_db)
-        connection_handle = pymongo.MongoClient(mongo_connection_uri)
+        self.connection_handle = get_mongo_connection_handle_url(self.host, self.port)
         # connection_handle[self.admin_db].command("dropUser", self.username)
-        connection_handle[self.admin_db].command("createUser", self.username, pwd=self.password,
-                                                 roles=["userAdminAnyDatabase"])
-        connection_handle.close()
+        # connection_handle[self.admin_db].command("createUser", self.username, pwd=self.password,
+        #                                          roles=["userAdminAnyDatabase"])
+        # connection_handle.close()
 
         original_document = {
             '_id': '16261B7E16FD9AC7CB79D640736000F3C9FE2122',
@@ -78,19 +74,19 @@ class TestCorrectAssembly(TestCase):
             'version' : 1,
             'createdDate' : datetime.fromisoformat('2019-07-08T03:12:59.352')
         }
-        self.connection_handle = get_mongo_connection_handle(username=self.username, password=self.password,
-                                                             host=self.host, authentication_database=self.admin_db)
+        # self.connection_handle = get_mongo_connection_handle(username=self.username, password=self.password,
+        #                                                      host=self.host, authentication_database=self.admin_db)
         self.connection_handle[self.eva_database][self.variant_collection].drop()
         self.connection_handle[self.eva_database][self.variant_collection].insert_many([
             original_document, horse_doc, another_horse_doc, horse_already_genbank])
 
     def tearDown(self) -> None:
         self.connection_handle[self.eva_database][self.variant_collection].drop()
-        self.connection_handle[self.admin_db].command("dropUser", self.username)
+        # self.connection_handle[self.admin_db].command("dropUser", self.username)
         self.connection_handle.close()
 
     def test_correct(self):
-        fixed = correct(self.username, self.password, self.host, studies=['PRJEB30080'],
+        fixed = correct(None, None, self.host, studies=['PRJEB30080'],
                         assembly_accession='GCA_000181335.3')
         self.assertEqual(fixed, 1)
         variant = (self.connection_handle[self.eva_database][self.variant_collection].find_one({'seq': 'GCA_000181335.3'}))
@@ -100,7 +96,7 @@ class TestCorrectAssembly(TestCase):
         self.assertIsNone(variant)
 
     def test_already_genbank(self):
-        fixed = correct(self.username, self.password, self.host, studies=['PRJEB9799', 'PRJEB24630', 'PRJEB27771'],
+        fixed = correct(None, None, self.host, studies=['PRJEB9799', 'PRJEB24630', 'PRJEB27771'],
                         assembly_accession='GCA_000002305.1')
         self.assertEqual(fixed, 2)
         variants = (self.connection_handle[self.eva_database][self.variant_collection].find({'seq': 'GCA_000002305.1'}))
@@ -111,7 +107,7 @@ class TestCorrectAssembly(TestCase):
         self.assertIsNone(variant)
 
     def test_ignore_otherstudies(self):
-        fixed = correct(self.username, self.password, self.host, studies=['PRJEB9799'],
+        fixed = correct(None, None, self.host, studies=['PRJEB9799'],
                         assembly_accession='GCA_000002305.1')
         self.assertEqual(fixed, 1)
         variants = (self.connection_handle[self.eva_database][self.variant_collection].find({'seq': 'GCA_000002305.1'}))
@@ -119,7 +115,7 @@ class TestCorrectAssembly(TestCase):
             self.assertTrue(variant['contig'] in ['CM000377.2', '21', 'CM000387.2'])
 
     def test_chunk_size(self):
-        fixed = correct(self.username, self.password, self.host, studies=['PRJEB9799', 'PRJEB24630', 'PRJEB27771'],
+        fixed = correct(None, None, self.host, studies=['PRJEB9799', 'PRJEB24630', 'PRJEB27771'],
                         assembly_accession='GCA_000002305.1', chunk_size=1)
         self.assertEqual(fixed, 2)
         variants = (self.connection_handle[self.eva_database][self.variant_collection].find({'seq': 'GCA_000002305.1'}))
@@ -128,3 +124,29 @@ class TestCorrectAssembly(TestCase):
         variant = (self.connection_handle[self.eva_database][self.variant_collection].find_one(
             {'seq': 'GCA_000181335.3', 'contig': '1'}))
         self.assertIsNone(variant)
+
+    def test_can_not_replace_contig(self):
+        unreplaceable_doc = {
+            '_id': 'B0B2176A78EDD8B29FEC60E87941287DC9E5056B',
+            'seq': 'GCA_000002305.1',
+            'tax': 9796,
+            'study': 'PRJEB27771',
+            'contig': 'asdf',
+            'start': 30681601,
+            'ref': 'C',
+            'alt': 'T',
+            'accession': 5081976263,
+            'version': 1,
+            'createdDate': datetime.fromisoformat('2018-09-20T14:31:26.164')
+        }
+        self.connection_handle[self.eva_database][self.variant_collection].insert_one(unreplaceable_doc)
+        try:
+            fixed = correct(None, None, self.host, studies=['PRJEB9799', 'PRJEB24630', 'PRJEB27771'],
+                            assembly_accession='GCA_000002305.1')
+            self.fail("expected an exception but none was thrown")
+        except Exception:
+            variants = (self.connection_handle[self.eva_database][self.variant_collection].find(
+                {'seq': 'GCA_000002305.1'}))
+            original_contigs_before_replacement = ['CM000387.2', '1', '21', 'asdf']
+            for variant in variants:
+                self.assertIn(variant['contig'], original_contigs_before_replacement)
