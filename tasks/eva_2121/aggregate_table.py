a52571409b8405a7252ac2ef5b9b8bd6ab343bdc
Timothee Cezard <tcezard@ebi.ac.uk>
2020-08-25 09:13:18 +0100

    Switch to using the download executable in the cluster

--- tasks/eva_2121/aggregate_table.py
+++ tasks/eva_2121/aggregate_table.py
@@ -1,13 +1,43 @@
 #!/usr/bin/env python
 import csv
 import glob
+import logging
 import os
+import subprocess
+import sys
 from argparse import ArgumentParser
 from collections import defaultdict
 
-from tasks.eva_2121.assembly_downloader import download_assembly
-
 total_number_variant = 0
+eva_accession_path = ''
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(asctime)-15s %(levelname)s %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def run_command_with_output(command_description, command, return_process_output=False):
+    process_output = ""
+
+    logger.info("Starting process: " + command_description)
+    logger.info("Running command: " + command)
+
+    with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=1, universal_newlines=True,
+                          shell=True) as process:
+        for line in iter(process.stdout.readline, ''):
+            line = str(line).rstrip()
+            logger.info(line)
+            if return_process_output:
+                process_output += line + "\n"
+        for line in iter(process.stderr.readline, ''):
+            line = str(line).rstrip()
+            logger.error(line)
+    if process.returncode != 0:
+        logger.error(command_description + " failed! Refer to the error messages for details.")
+        raise subprocess.CalledProcessError(process.returncode, process.args)
+    else:
+        logger.info(command_description + " - completed successfully")
+    if return_process_output:
+        return process_output
 
 
 def parse_input(input_file):
@@ -26,33 +56,6 @@ def parse_input(input_file):
     return data_per_taxid, data_per_taxid_and_assembly
 
 
-def find_reference_and_assembly_report(species_name, accession, assembly_dir):
-    fasta, report = None, None
-    accession_dir = os.path.join(
-        assembly_dir,
-        species_name.lower().replace(' ', '_'),
-        accession
-    )
-    os.makedirs(accession_dir, exist_ok=True)
-    fasta, report = download_assembly(assembly_dir,accession)
-    # fastas = glob.glob(os.path.join(accession_dir, '*.fa'))
-    # nucleotide_fastas = glob.glob(os.path.join(accession_dir, '*.fna'))
-    # reports = glob.glob(os.path.join(accession_dir, '*_assembly_report.txt'))
-    # if fastas and len(fastas) == 1:
-    #     fasta = fastas[0]
-    # elif nucleotide_fastas and len(nucleotide_fastas) == 1:
-    #     fasta = nucleotide_fastas[0]
-    # else:
-    #     print('Found %s fasta file with pattern: %s' % (len(fastas) + len(nucleotide_fastas), os.path.join(accession_dir, '*.f?a')))
-    # if not reports:
-    #     report, fasta = download_assembly(detination_dir=accession_dir, assembly_accession=accession)
-    # elif reports:
-    #     report = reports[0]
-    # if report:
-    #     print('Found %s assembly report file with pattern: %s' % (len(reports), os.path.join(accession_dir, '*_assembly_report.txt')))
-    return fasta, report
-
-
 def assign_tempmongo_host(number_variants):
     global total_number_variant
     total_number_variant += number_variants
@@ -60,7 +63,45 @@ def assign_tempmongo_host(number_variants):
     return 'tempmongo-' + str(instance_number)
 
 
-def aggregate_list_of_species(input_file, assembly_dir):
+def retrieve_assembly(scientific_name, assembly_accession, assembly_dir, download_dir):
+    fasta, report = None, None
+    accession_dir = os.path.join(
+        assembly_dir,
+        scientific_name.lower().replace(' ', '_'),
+        assembly_accession
+    )
+
+    fastas = glob.glob(os.path.join(accession_dir, '*.fa'))
+    nucleotide_fastas = glob.glob(os.path.join(accession_dir, '*.fna'))
+    reports = glob.glob(os.path.join(accession_dir, '*_assembly_report.txt'))
+    if fastas and len(fastas) == 1:
+        fasta = fastas[0]
+    elif nucleotide_fastas and len(nucleotide_fastas) == 1:
+        fasta = nucleotide_fastas[0]
+    if reports and len(reports) == 1:
+        report = reports[0]
+
+    if fasta is None or report is None:
+        report, fasta = download_assembly(scientific_name, assembly_accession, download_dir)
+    return fasta, report
+
+
+def download_assembly(scientific_name, assembly_accession, download_dir, eva_accession_path):
+    python = os.path.join(eva_accession_path, "python-import-automation/bin/python")
+    genome_downloader = os.path.join(eva_accession_path, "eva-accession/eva-accession-import-automation/genome_downloader.py")
+    private_json = os.path.join(eva_accession_path, "private-config.json")
+
+    output_dir = os.path.join(download_dir, scientific_name.lower().replace(' ', '_'))
+    os.makedirs(output_dir, exist_ok=True)
+    command = "{} {} -p {} -a {} -o {}""".format(python, genome_downloader, private_json, assembly_accession, output_dir)
+    run_command_with_output('Retrieve assembly fasta and assembly report', command)
+
+    assembly_report = glob.glob(os.path.join(output_dir, assembly_accession, "*_assembly_report.txt"))[0]
+    assembly_fasta = os.path.join(output_dir, assembly_accession, assembly_accession + ".fa")
+    return assembly_fasta, assembly_report
+
+
+def aggregate_list_of_species(input_file, assembly_dir, download_dir, output_tsv):
     """
     """
     data_per_taxid, data_per_taxid_and_assembly = parse_input(input_file)
@@ -86,44 +127,54 @@ def aggregate_list_of_species(input_file, assembly_dir):
             dbsnps_only.append(taxid)
         else:
             mixture.append(taxid)
+    with open(output_tsv, 'w') as open_output:
+        headers = ['taxonomy_id', 'scientific_name', 'assembly', 'sources', 'fasta_path', 'report_path', 'temp_mongo',
+                   'number_variants', 'to_process']
+        print('\t'.join([str(o) for o in headers]), file=open_output)
+        for taxid, assembly in data_per_taxid_and_assembly:
+            rows = data_per_taxid_and_assembly[(taxid, assembly)]
+            scientific_name = {row['Scientific Name From Taxid'] for row in rows}
+            assert len(scientific_name) == 1
+            scientific_name = scientific_name.pop()
+            number_variants = sum([int(row['Number Of Variants (submitted variants)'].replace(',', '')) for row in rows])
 
-    print(len(only_unmapped_tax_id), only_unmapped_tax_id)
-    print(len(unchanged_taxid), unchanged_taxid)
-    print(len(dbsnps_only), dbsnps_only)
-    print(len(eva_only), eva_only)
-    print(len(mixture), mixture)
+            if taxid in unchanged_taxid + only_unmapped_tax_id or assembly is 'Unmapped':
+                to_process = 'no'
+                fasta, report, temp_mongo = '', '', ''
+            else:
+                to_process = 'yes'
+                fasta, report = retrieve_assembly(scientific_name, assembly, assembly_dir, download_dir)
+                temp_mongo = assign_tempmongo_host(number_variants)
 
-    for taxid, assembly in data_per_taxid_and_assembly:
-        if taxid not in mixture + eva_only + dbsnps_only:
-            continue
-        rows = data_per_taxid_and_assembly[(taxid, assembly)]
-        scientific_name = {row['Scientific Name From Taxid'] for row in rows}
-        assert len(scientific_name) == 1
-        scientific_name = scientific_name.pop()
-        if assembly != 'Unmapped':
-            fasta, report = find_reference_and_assembly_report(scientific_name, assembly, assembly_dir)
-        if not report:
-            fasta, report = None, None
-        number_variants = sum([int(row['Number Of Variants (submitted variants)'].replace(',', '')) for row in rows])
-        out = [
-            taxid,
-            scientific_name,
-            assembly,
-            ','.join({row['Source'] for row in rows}),
-            fasta,
-            report,
-            assign_tempmongo_host(number_variants)
-        ]
-        print('\t'.join([str(o) for o in out]))
+            out = [
+                taxid,
+                scientific_name,
+                assembly,
+                ','.join({row['Source'] for row in rows}),
+                fasta,
+                report,
+                temp_mongo,
+                number_variants,
+                to_process
+            ]
+            print('\t'.join([str(o) for o in out]), file=open_output)
 
 
 def main():
     argparse = ArgumentParser()
     argparse.add_argument('--input', help='Path to the file containing the taxonomies and assemblies', required=True)
-    argparse.add_argument('--assembly_dir', help='Path to the directory containing all species assemblies', required=True)
+    argparse.add_argument('--assembly_dir', help='Path to the directory containing pre-downloaded species assemblies', required=True)
+    argparse.add_argument('--download_dir', help='Path to the directory where additional containing species assemblies will be downloaded', required=True)
+    argparse.add_argument('--output_tsv', help='Path to the tsv file that will contain the list of species to process', required=True)
+    argparse.add_argument('--eva_accession_path', help='')
+
     args = argparse.parse_args()
+
+    global eva_accession_path
+    if args.eva_accession_path:
+        eva_accession_path = args.eva_accession_path
     
-    aggregate_list_of_species(args.input, args.assembly_dir)
+    aggregate_list_of_species(args.input, args.assembly_dir, args.download_dir, args.output_tsv)
 
 
 if __name__ == "__main__":
