5411bd9a1187140480efd04fa159bd0b4aad0123
Timothee Cezard <tcezard@ebi.ac.uk>
2021-04-09 11:53:41 +0100

    Explicit output file in extract_AF.py Flip axis of graphs of variant per sample

--- tasks/eva_2373/extract_AF.py
+++ tasks/eva_2373/extract_AF.py
@@ -3,9 +3,10 @@ from argparse import ArgumentParser
 import pysam
 
 
-def process_files(vcf_files_list):
+def process_files(vcf_files_list, output_tsv):
     """Extract Basic info from all the VCF files provided to populate a tsv file"""
-    with open(vcf_files_list) as open_list:
+
+    with open(vcf_files_list) as open_list, open(output_tsv, 'w') as open_output:
         for file_name in open_list:
             sample_name = file_name.split('.')[0]
             with pysam.VariantFile(file_name.strip(), 'r') as vcf_in:
@@ -13,15 +14,17 @@ def process_files(vcf_files_list):
                     variant_type='SNP' if len(vcf_record.ref) == 1 and len(vcf_record.alts[0]) == 1 else 'INDEL'
                     print(
                         ':'.join((vcf_record.chrom, str(vcf_record.pos), vcf_record.ref, vcf_record.alts[0]))
-                        + f"\t{vcf_record.info['AF']}\t{variant_type}\t{sample_name}"
+                        + f"\t{vcf_record.info['AF']}\t{variant_type}\t{sample_name}", file=open_output
                     )
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('--vcf_files_list')
+    parser.add_argument('--output_tsv')
     args = parser.parse_args()
 
-    process_files(args.vcf_files_list)
+    process_files(args.vcf_files_list, args.output_tsv)
+
 
 
