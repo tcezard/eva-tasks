e38aa765fc53e36d37d599957e59b647335d4cff
Andres Silva <afsilva04@gmail.com>
2021-05-05 13:02:18 +0100

    address review comments

--- tasks/eva_2407/load_to_contig_alias_db.py
+++ tasks/eva_2407/load_to_contig_alias_db.py
@@ -1,6 +1,5 @@
 import argparse
 import base64
-import sys
 import urllib.request
 
 import psycopg2
@@ -32,7 +31,7 @@ def get_contig_alias_auth(private_config_xml_file):
 
 
 @retry(tries=4, delay=2, backoff=1.2, jitter=(1, 3))
-def call_admin_endpoint(assemblies, admin_credentials):
+def load_assembly_to_contig_alias(assemblies, admin_credentials):
     for assembly in assemblies:
         url = f"https://www.ebi.ac.uk/eva/webservices/contig-alias/v1/admin/assemblies/{assembly}"
         headers = {'Authorization': 'Basic ' + admin_credentials}
@@ -48,18 +47,12 @@ def load_data_to_contig_alias(private_config_xml_file, assembly_list):
     assemblies = assembly_list if assembly_list else get_assemblies_from_evapro(private_config_xml_file)
     admin_credentials = get_contig_alias_auth(private_config_xml_file)
     logger.info(f"Assemblies to be loaded into contig alias database: {assemblies}")
-    call_admin_endpoint(assemblies, admin_credentials)
+    load_assembly_to_contig_alias(assemblies, admin_credentials)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Get stats from variant warehouse', add_help=False)
+    parser = argparse.ArgumentParser(description='Load data into contig alias database', add_help=False)
     parser.add_argument("--private-config-xml-file", help="ex: /path/to/eva-maven-settings.xml", required=True)
     parser.add_argument("--assembly-list", help="Assembly list e.g. GCA_000181335.4", required=False, nargs='+')
-    args = {}
-    try:
-        args = parser.parse_args()
-        load_data_to_contig_alias(args.private_config_xml_file, args.assembly_list)
-    except Exception as ex:
-        logger.exception(ex)
-        sys.exit(1)
-    sys.exit(0)
+    args = parser.parse_args()
+    load_data_to_contig_alias(args.private_config_xml_file, args.assembly_list)
