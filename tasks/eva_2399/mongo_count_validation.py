df958d5d016edc2cde2294063e33192f368ed543
nitin-ebi <79518737+nitin-ebi@users.noreply.github.com>
2021-05-07 13:40:49 +0100

    EVA-2400 - Mongo DB Document Count Validation - Script Update (#36)
    * changed script to get documents counts from a single mongo source

--- tasks/eva_2399/mongo_count_validation.py
+++ tasks/eva_2399/mongo_count_validation.py
@@ -15,47 +15,26 @@ logging_config.add_stdout_handler()
 mongo_migration_count_validation_table_name = "eva_tasks.mongo4_migration_count_validation"
 
 
-def create_collection_count_validation_report(mongo_source: MongoDatabase, mongo_dest: MongoDatabase, database_list):
+def create_collection_count_validation_report(mongo_source: MongoDatabase, database_list):
     count_validation_res_list = []
     report_timestamp = datetime.now()
+    mongo_host = mongo_source.mongo_handle.address[0]
 
     for db in database_list:
         mongo_source.db_name = db
-        mongo_dest.db_name = db
         source_collections = mongo_source.get_collection_names()
-        dest_collections = mongo_dest.get_collection_names()
 
-        if not source_collections and not dest_collections:
-            logger.error(f"database {db} does not exist in either of mongo instances")
+        if not source_collections:
+            logger.warning(f"database {db} does not exist in mongo instances {mongo_host}")
             continue
 
-        all_collections = sorted(set(source_collections).union(set(dest_collections)))
+        for coll in sorted(source_collections):
+            logger.info(f"fetching count for database ({db}) - collection ({coll})")
 
-        for coll in all_collections:
-            logger.info(f"fetching count for collection ({coll}) in database ({db})")
+            no_of_documents = get_documents_count_for_collection(mongo_source, db, coll)
+            logger.info(f"Found {no_of_documents} documents in database ({db}) - collection ({coll})")
 
-            if coll not in source_collections:
-                logger.error(f"collection ({coll}) does not exist in database ({db}) in mongo source")
-                no_of_documents_in_src = -1
-            else:
-                no_of_documents_in_src = get_documents_count_for_collection(mongo_source, db, coll)
-
-            if coll not in dest_collections:
-                logger.error(f"collection ({coll}) does not exist in database ({db}) in mongo destination")
-                no_of_documents_in_dest = -1
-            else:
-                no_of_documents_in_dest = get_documents_count_for_collection(mongo_dest, db, coll)
-
-            if no_of_documents_in_src != no_of_documents_in_dest and \
-                    no_of_documents_in_src != -1 and no_of_documents_in_dest != -1:
-                logger.error(f"no of documents in collection ({coll}) of database ({db}) does not match : "
-                             f"source has ({no_of_documents_in_src}) documents whereas destination has ({no_of_documents_in_dest})")
-            elif no_of_documents_in_src == no_of_documents_in_dest:
-                logger.info(
-                    f"Both source and destination has same no of records ({no_of_documents_in_src}) in collection {coll} of database {db}")
-
-            count_validation_res_list.append(
-                [db, coll, no_of_documents_in_src, no_of_documents_in_dest, report_timestamp])
+            count_validation_res_list.append([mongo_host, db, coll, no_of_documents, report_timestamp])
 
     return count_validation_res_list
 
@@ -69,9 +48,9 @@ def create_table_for_count_validation(private_config_xml_file):
     with psycopg2.connect(get_pg_metadata_uri_for_eva_profile("development", private_config_xml_file),
                           user="evadev") as metadata_connection_handle:
         query_create_table_for_count_validation = "create table if not exists {0} " \
-                                                  "(database text, collection text, source_document_count bigint not null, " \
-                                                  "destination_document_count bigint not null, report_time timestamp, " \
-                                                  "primary key(database, collection, report_time))" \
+                                                  "(mongo_host text, database text, collection text, " \
+                                                  "document_count bigint not null, report_time timestamp, " \
+                                                  "primary key(mongo_host, database, collection, report_time))" \
             .format(mongo_migration_count_validation_table_name)
 
     execute_query(metadata_connection_handle, query_create_table_for_count_validation)
@@ -84,7 +63,7 @@ def insert_count_validation_result_to_db(private_config_xml_file, count_validati
             with metadata_connection_handle.cursor() as cursor:
                 psycopg2.extras.execute_values(cursor,
                                                "INSERT INTO {0} "
-                                               "(database, collection, source_document_count, destination_document_count,report_time) "
+                                               "(mongo_host, database, collection, document_count,report_time) "
                                                "VALUES %s".format(mongo_migration_count_validation_table_name),
                                                [tuple(x) for x in count_validation_res_list])
 
@@ -105,19 +84,12 @@ def get_databases_list_for_validation(file_path):
 
 def main():
     parser: ArgumentParser = argparse.ArgumentParser(
-        description='Given two different mongo db instances and a list of databases, compare number of documents '
-                    'in each of the corresponding collections for all the given dbs in two instances',
+        description='Given a list of databases, calculate number of documents in each collection of each of the given database and store the report',
         formatter_class=argparse.RawTextHelpFormatter, add_help=False)
     parser.add_argument("--mongo-source-uri",
                         help="Mongo Source URI (ex: mongodb://user:@mongos-source-host:27017/admin)", required=True)
     parser.add_argument("--mongo-source-secrets-file",
                         help="Full path to the Mongo Source secrets file (ex: /path/to/mongo/source/secret)",
-                        required=True)
-    parser.add_argument("--mongo-dest-uri",
-                        help="Mongo Destination URI (ex: mongodb://user:@mongos-dest-host:27017/admin)",
-                        required=True)
-    parser.add_argument("--mongo-dest-secrets-file",
-                        help="Full path to the Mongo Destination secrets file (ex: /path/to/mongo/dest/secret)",
                         required=True)
     parser.add_argument("--db-list",
                         help="Full path to the File containing list of Databases (ex: eva_hsapiens_grch37)",
@@ -129,12 +101,11 @@ def main():
 
     args = parser.parse_args()
 
-    source_mongo = MongoDatabase(uri=args.mongo_source_uri, secrets_file=args.mongo_source_secrets_file)
-    dest_mongo = MongoDatabase(uri=args.mongo_dest_uri, secrets_file=args.mongo_dest_secrets_file)
+    mongo_source = MongoDatabase(uri=args.mongo_source_uri, secrets_file=args.mongo_source_secrets_file)
     database_list = get_databases_list_for_validation(args.db_list)
 
     create_table_for_count_validation(args.private_config_xml_file)
-    count_validation_result = create_collection_count_validation_report(source_mongo, dest_mongo, database_list)
+    count_validation_result = create_collection_count_validation_report(mongo_source, database_list)
     insert_count_validation_result_to_db(args.private_config_xml_file, count_validation_result)
 
 
