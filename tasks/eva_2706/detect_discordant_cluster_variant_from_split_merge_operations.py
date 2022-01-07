40f3f17d16758c05fade592e095e76ba61ee7c00
Timothee Cezard <tcezard@ebi.ac.uk>
2022-01-07 17:02:38 +0000

    Add script to detect discordant clustered variant that are involve in a merge or split event

--- tasks/eva_2706/detect_discordant_cluster_variant_from_split_merge_operations.py
+++ tasks/eva_2706/detect_discordant_cluster_variant_from_split_merge_operations.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 from collections import defaultdict
 from itertools import zip_longest
 
@@ -28,38 +29,82 @@ def compare(clustered_variants, submitted_variant_position_per_rs):
 
         positions = submitted_variant_position_per_rs[clustered_variant['accession']]
         if len(positions) > 1:
-            # This actually happened in dbSNP a but is  a different error.
-            logger.warning(f'{len(positions)} positions found in submitted variants for '
-                           f'rs{clustered_variant["accession"]}. These are {", ".join(positions)}')
+            logger.error(f'{len(positions)} positions found in submitted variants for '
+                         f'rs{clustered_variant["accession"]}')
+            error += 1
+            continue
 
         if pos not in positions:
-            logger.error(f'cluster position ({pos}) not found in submitted position ({", ".join(positions)}) '
+            logger.error(f'cluster position ({pos}) different from submitted position ({positions.pop()}) '
                          f'for rs{clustered_variant["accession"]}')
             error += 1
             continue
     return error
 
 
-def detect_discordant_cluster_variant(mongo_source, assemblies, batch_size=1000):
+NO_SEQUENCE_ALTERATION = 'NO_SEQUENCE_ALTERATION'
+SNV = 'SNV'
+MNV = 'MNV'
+DEL = 'DEL'
+INS = 'INS'
+
+
+def classify_variant(reference, alternate):
+    reference = reference.trim().upper()
+    alternate = alternate.trim().upper()
+
+    if reference == "NOVARIATION" or alternate == reference:
+        return NO_SEQUENCE_ALTERATION
+    else:
+        ''.isalpha()
+        is_ref_alpha = reference.isalpha()
+        is_alt_alpha = alternate.isalpha()
+
+        if is_ref_alpha and is_alt_alpha:
+            if len(reference) == len(alternate):
+                return SNV if len(reference) == 1 else MNV
+        if is_ref_alpha and not alternate:
+            return DEL
+        if is_alt_alpha  and not reference:
+            return INS
+
+
+def submitted_variant_to_clustered_variant_hash(submitted_variant):
+    """Calculate the SHA1 digest from the clustered variant based on the submitted variant information"""
+    h = hashlib.sha1()
+    h.update('_'.join([
+        submitted_variant.get('seq'),
+        submitted_variant.get('contig'),
+        submitted_variant.get('start'),
+        classify_variant(submitted_variant.get('ref'), submitted_variant.get('alt')),
+    ]).encode())
+    return h.hexdigest().upper()
+
+
+def detect_discordant_cluster_variant_from_split_merge_operations(mongo_source, assemblies, batch_size=1000):
+    sveo_collection = mongo_source.mongo_handle[mongo_source.db_name]["submittedVariantOperationEntity"]
     dbsnp_cve_collection = mongo_source.mongo_handle[mongo_source.db_name]["dbsnpClusteredVariantEntity"]
     dbsnp_sve_collection = mongo_source.mongo_handle[mongo_source.db_name]["dbsnpSubmittedVariantEntity"]
-    cve_filter_criteria = {}
-    sve_filter_criteria = {}
+    sveo_filter_criteria = {'eventType': {'$is': ['RS_MERGE_CANDIDATES', 'RS_SPLIT_CANDIDATES']}}
     if assemblies:
-        cve_filter_criteria = {'asm': {"$in": assemblies}}
-        sve_filter_criteria = {'seq': {"$in": assemblies}}
-    cursor = dbsnp_cve_collection.with_options(read_concern=ReadConcern("majority"))\
-                                 .find(cve_filter_criteria, no_cursor_timeout=True)
+        sveo_filter_criteria['seq'] = {"$in": assemblies}
+    cursor = sveo_collection.with_options(read_concern=ReadConcern("majority"))\
+                            .find(sveo_filter_criteria, no_cursor_timeout=True)
     cursor.batch_size(batch_size)
     projection = {'contig': 1, 'start': 1, 'rs': 1}
     nb_clustered_variants = 0
     nb_error = 0
-    for clustered_variants in grouper(batch_size, cursor):
+    for submitted_variant_operations in grouper(batch_size, cursor):
+        clustered_variant_hashes = set((
+            submitted_variant_to_clustered_variant_hash(submitted_variant)
+            for submitted_variant_operation in submitted_variant_operations
+            for submitted_variant in submitted_variant_operation if submitted_variant_operation
+        ))
+        clustered_variants = dbsnp_cve_collection.find({'id': {'$in': clustered_variant_hashes}})
         rsids = [clustered_variant.get('accession') for clustered_variant in clustered_variants if clustered_variant]
         nb_clustered_variants += len(rsids)
-        sve_filtering = sve_filter_criteria.copy()
-        sve_filtering['rs'] = {'$in': rsids}
-        sve_cursor = dbsnp_sve_collection.with_options(read_concern=ReadConcern("majority"))\
+        sve_filtering = {'seq': {"$in": assemblies}, 'rs': {'$in': rsids}}
+        sve_cursor = dbsnp_sve_collection.with_options(read_concern=ReadConcern("majority")) \
                                          .find(sve_filtering, projection)
         submitted_variant_position_per_rs = defaultdict(set)
         for sve in sve_cursor:
@@ -71,7 +116,8 @@ def detect_discordant_cluster_variant(mongo_source, assemblies, batch_size=1000)
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Detect clustered variant that have position discordant with their submitted variants')
+        description='Detect clustered variant that have position discordant with their submitted variants and are '
+                    'involve in a merge or split event.')
     parser.add_argument("--mongo-source-uri",
                         help="Mongo Source URI (ex: mongodb://user:@mongos-source-host:27017/admin)", required=True)
     parser.add_argument("--mongo-source-secrets-file",
@@ -82,7 +128,7 @@ def main():
     args = parser.parse_args()
     mongo_source = MongoDatabase(uri=args.mongo_source_uri, secrets_file=args.mongo_source_secrets_file,
                                  db_name="eva_accession_sharded")
-    detect_discordant_cluster_variant(mongo_source, args.assemblies, args.batch_size)
+    detect_discordant_cluster_variant_from_split_merge_operations(mongo_source, args.assemblies, args.batch_size)
 
 
 if __name__ == "__main__":
