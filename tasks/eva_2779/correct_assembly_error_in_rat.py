a8cea1e548c1e94a97e964889867828d2bcf7d08
Timothee Cezard <tcezard@ebi.ac.uk>
2022-03-24 10:28:31 +0000

    Script to correct assembly accession and MT contig name in rat

--- tasks/eva_2779/correct_assembly_error_in_rat.py
+++ tasks/eva_2779/correct_assembly_error_in_rat.py
@@ -24,68 +24,244 @@ def get_submitted_SHA1(variant_rec):
     return get_SHA1('_'.join([str(variant_rec[key]) for key in keys]))
 
 
+def get_clustered_SHA1(variant_rec):
+    """Calculate the SHA1 digest from the asm, contig, start and type attributes of the variant"""
+    keys = ['asm', 'contig', 'start', 'type']
+    return get_SHA1('_'.join([str(variant_rec[key]) for key in keys]))
+
+
 def find_documents_in_batch(mongo_source, collection_name, filter_criteria, batch_size=1000):
     collection = mongo_source.mongo_handle[mongo_source.db_name][collection_name]
     cursor = collection.with_options(read_concern=ReadConcern("majority")) \
                        .find(filter_criteria, no_cursor_timeout=True)
     records = []
-    for result in cursor:
-        records.append(result)
-        if len(records) == batch_size:
-            yield records
-            records = []
-    yield records
-
-def replace_with_correct_assembly(mongo_source, filter_criteria, correction_map):
-    correction_map = {'seq': 'GCA_015227675.2'}
-    filter_criteria = {'seq': 'GCA_015227675.1'}
-
-    sve_collection = mongo_source.mongo_handle[mongo_source.db_name]["submittedVariantEntity"]
-    cursor = sve_collection.with_options(read_concern=ReadConcern("majority")) \
-                           .find(filter_criteria, no_cursor_timeout=True)
-    insert_statements = []
-    drop_statements = []
-    number_of_variants_to_replace = 1
-    total_inserted, total_dropped = 0, 0
     try:
-        for variant in cursor:
-            original_id = get_SHA1(variant)
-            assert variant['_id'] == original_id, "Original id is different from the one calculated %s != %s" % (
-                variant['_id'], original_id)
-            for key in correction_map:
-                variant[key] = correction_map[key]
-            variant['_id'] = get_submitted_SHA1(variant)
-            insert_statements.append(pymongo.InsertOne(variant))
-            drop_statements.append(pymongo.DeleteOne({'_id': original_id}))
-        result_insert = sve_collection.with_options(write_concern=WriteConcern(w="majority", wtimeout=1200000))\
-                                      .bulk_write(requests=insert_statements, ordered=False)
-        total_inserted += result_insert.inserted_count
-        result_drop = sve_collection.with_options(write_concern=WriteConcern(w="majority", wtimeout=1200000)) \
-            .bulk_write(requests=drop_statements, ordered=False)
-        total_dropped += result_drop.deleted_count
-        logger.info('%s / %s new documents inserted' % (total_inserted, number_of_variants_to_replace))
-        logger.info('%s / %s old documents dropped' % (total_dropped, number_of_variants_to_replace))
+        for result in cursor:
+            records.append(result)
+            if len(records) == batch_size:
+                yield records
+                records = []
+        yield records
     except Exception as e:
-        print(traceback.format_exc())
+        logger.exception(traceback.format_exc())
         raise e
     finally:
         cursor.close()
+
+
+def replace_document_with_correct_information(mongo_source, collection_name, id_creation_func, filter_criteria,
+                                              correction_map, batch_size):
+    collection = mongo_source.mongo_handle[mongo_source.db_name][collection_name]
+
+    total_inserted, total_dropped = 0, 0
+    try:
+        for batch_of_variant in find_documents_in_batch(mongo_source, collection_name, filter_criteria, batch_size):
+            insert_statements = []
+            drop_statements = []
+            for variant in batch_of_variant:
+                original_id = id_creation_func(variant)
+                assert variant['_id'] == original_id, "Original id is different from the one calculated %s != %s" % (
+                    variant['_id'], original_id)
+                for key in correction_map:
+                    variant[key] = correction_map[key]
+                variant['_id'] = id_creation_func(variant)
+                insert_statements.append(pymongo.InsertOne(variant))
+                drop_statements.append(pymongo.DeleteOne({'_id': original_id}))
+            result_insert = collection.with_options(write_concern=WriteConcern(w="majority", wtimeout=1200000))\
+                                      .bulk_write(requests=insert_statements, ordered=False)
+            total_inserted += result_insert.inserted_count
+            result_drop = collection.with_options(write_concern=WriteConcern(w="majority", wtimeout=1200000)) \
+                                    .bulk_write(requests=drop_statements, ordered=False)
+            total_dropped += result_drop.deleted_count
+        logger.info(f'{total_inserted} new documents inserted in {collection_name}')
+        logger.info(f'{total_dropped} old documents dropped in {collection_name}')
+    except Exception as e:
+        print(traceback.format_exc())
+        raise e
     return total_inserted
+
+
+def update_operation_entities(mongo_source, collection_name, id_creation_func, filter_criteria, correction_map, batch_size):
+    collection = mongo_source.mongo_handle[mongo_source.db_name][collection_name]
+    total_updated = 0
+    try:
+        for batch_of_variant in find_documents_in_batch(mongo_source, collection_name, filter_criteria, batch_size):
+            update_statements = []
+            for variant in batch_of_variant:
+                filter_dict = {'_id': variant['_id']}
+                filter_dict.update(filter_criteria)
+                for key in correction_map:
+                    if callable(correction_map[key]):
+                        variant[key] = correction_map[key](variant[key])
+                    elif 'inactiveObjects' in key:
+                        inactive_objects = variant['inactiveObjects']
+                        prop = key.split('.')[-1]
+                        for inactive in inactive_objects:
+                            inactive[prop] = correction_map[key]
+                    else:
+                        variant[key] = correction_map[key]
+                for inactive in variant['inactiveObjects']:
+                    inactive['hashedMessage'] = id_creation_func(inactive)
+                variant.pop('_id')
+                update_statements.append(pymongo.ReplaceOne(filter_dict, variant))
+            result_update = collection.with_options(write_concern=WriteConcern(w="majority", wtimeout=1200000)) \
+                .bulk_write(requests=update_statements, ordered=False)
+            total_updated += result_update.modified_count
+        logger.info(f'{total_updated} documents updated in {collection_name}')
+    except Exception as e:
+        print(traceback.format_exc())
+        raise e
+    return total_updated
+
+
+def replace_variant_entities(mongo_source, batch_size):
+    # submitted variants on MT chromosome
+    replace_document_with_correct_information(
+        mongo_source, "submittedVariantEntity", get_submitted_SHA1,
+        {'seq': 'GCA_015227675.1', 'contig': 'CM026996.1'},
+        {'seq': 'GCA_015227675.2', 'contig': 'AY172581.1'},
+        batch_size
+    )
+    replace_document_with_correct_information(
+        mongo_source, "dbsnpSubmittedVariantEntity", get_submitted_SHA1,
+        {'seq': 'GCA_015227675.1', 'contig': 'CM026996.1'},
+        {'seq': 'GCA_015227675.2', 'contig': 'AY172581.1'},
+        batch_size
+    )
+
+    # submitted variants Not on MT chromosome
+    replace_document_with_correct_information(
+        mongo_source, "submittedVariantEntity", get_submitted_SHA1, {'seq': 'GCA_015227675.1'},
+        {'seq': 'GCA_015227675.2'}, batch_size
+    )
+    replace_document_with_correct_information(
+        mongo_source, "dbsnpSubmittedVariantEntity", get_submitted_SHA1, {'seq': 'GCA_015227675.1'},
+        {'seq': 'GCA_015227675.2'}, batch_size
+    )
+
+    # Clustered variants on MT chromosome
+    replace_document_with_correct_information(
+        mongo_source, "clusteredVariantEntity", get_clustered_SHA1,
+        {'asm': 'GCA_015227675.1', 'contig': 'CM026996.1'},
+        {'asm': 'GCA_015227675.2', 'contig': 'AY172581.1'},
+        batch_size
+    )
+    replace_document_with_correct_information(
+        mongo_source, "dbsnpClusteredVariantEntity", get_clustered_SHA1,
+        {'asm': 'GCA_015227675.1', 'contig': 'CM026996.1'},
+        {'asm': 'GCA_015227675.2', 'contig': 'AY172581.1'},
+        batch_size
+    )
+
+    # Clustered variants Not on MT chromosome
+    replace_document_with_correct_information(
+        mongo_source, "clusteredVariantEntity", get_clustered_SHA1, {'asm': 'GCA_015227675.1'},
+        {'asm': 'GCA_015227675.2'}, batch_size
+    )
+    replace_document_with_correct_information(
+        mongo_source, "dbsnpClusteredVariantEntity", get_clustered_SHA1, {'asm': 'GCA_015227675.1'},
+        {'asm': 'GCA_015227675.2'}, batch_size
+    )
+
+    # MERGED Operations on MT chromosomes
+    update_operation_entities(
+        mongo_source, 'dbsnpClusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'MERGED'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1', 'reason': lambda x: x.replace('GCA_015227675.1', 'GCA_015227675.2')},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'clusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'MERGED'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1', 'reason': lambda x: x.replace('GCA_015227675.1', 'GCA_015227675.2')},
+        batch_size
+    )
+
+    # MERGED Operations Not on MT chromosomes
+    update_operation_entities(
+        mongo_source, 'dbsnpClusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'eventType': 'MERGED'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'reason': lambda x: x.replace('GCA_015227675.1', 'GCA_015227675.2')},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'clusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'eventType': 'MERGED'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'reason': lambda x: x.replace('GCA_015227675.1', 'GCA_015227675.2')},
+        batch_size
+    )
+
+    # SPLIT Operations on MT chromosomes
+    update_operation_entities(
+        mongo_source, 'dbsnpClusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'RS_SPLIT'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1'},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'clusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'RS_SPLIT'},
+        {'inactiveObjects.asm': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1'},
+        batch_size
+    )
+
+    # SPLIT Operations Not on MT chromosomes
+    update_operation_entities(
+        mongo_source, 'dbsnpClusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'eventType': 'RS_SPLIT'},
+        {'inactiveObjects.asm': 'GCA_015227675.2'},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'clusteredVariantOperationEntity', get_clustered_SHA1,
+        {'inactiveObjects.asm': 'GCA_015227675.1', 'eventType': 'RS_SPLIT'},
+        {'inactiveObjects.asm': 'GCA_015227675.2'},
+        batch_size
+    )
+
+    # UPDATE Operations on MT chromosome
+    update_operation_entities(
+        mongo_source, 'dbsnpSubmittedVariantOperationEntity', get_submitted_SHA1,
+        {'inactiveObjects.seq': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'UPDATED'},
+        {'inactiveObjects.seq': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1'},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'submittedVariantOperationEntity', get_submitted_SHA1,
+        {'inactiveObjects.seq': 'GCA_015227675.1', 'inactiveObjects.contig': 'CM026996.1', 'eventType': 'UPDATED'},
+        {'inactiveObjects.seq': 'GCA_015227675.2', 'inactiveObjects.contig': 'AY172581.1'},
+        batch_size
+    )
+
+    # UPDATE Operations Not on MT chromosome
+    update_operation_entities(
+        mongo_source, 'dbsnpSubmittedVariantOperationEntity', get_submitted_SHA1,
+        {'inactiveObjects.seq': 'GCA_015227675.1', 'eventType': 'UPDATED'},
+        {'inactiveObjects.seq': 'GCA_015227675.2'},
+        batch_size
+    )
+    update_operation_entities(
+        mongo_source, 'submittedVariantOperationEntity', get_submitted_SHA1,
+        {'inactiveObjects.seq': 'GCA_015227675.1', 'eventType': 'UPDATED'},
+        {'inactiveObjects.seq': 'GCA_015227675.2'},
+        batch_size
+    )
 
 
 def main():
     parser = argparse.ArgumentParser(
-        description='Correct assembly error in study PRJEB36115 by replacing assembly PRJEB36115 with GCA_000002315.5',
+        description='Correct assembly error in assembly GCA_015227675.1 by replacing it GCA_015227675.2',
         add_help=False)
     parser.add_argument("--mongo-source-uri",
                         help="Mongo Source URI (ex: mongodb://user:@mongos-source-host:27017/admin)", required=True)
     parser.add_argument("--mongo-source-secrets-file",
                         help="Full path to the Mongo Source secrets file (ex: /path/to/mongo/source/secret)",
                         required=True)
+    parser.add_argument("--batch-size", help="number of document processed at once", required=False, default=1000)
     args = parser.parse_args()
     mongo_source = MongoDatabase(uri=args.mongo_source_uri, secrets_file=args.mongo_source_secrets_file,
                                  db_name="eva_accession_sharded")
-    replace_with_correct_assembly(mongo_source)
+    replace_variant_entities(mongo_source, batch_size=args.batch_size)
 
 
 if __name__ == "__main__":
---
+++ tasks/eva_2779/tests/test_correct_assembly_error_in_rat.py
@@ -0,0 +1,120 @@
+import hashlib
+
+from ebi_eva_common_pyutils.mongodb import MongoDatabase
+from unittest import TestCase
+
+from tasks.eva_2779.correct_assembly_error_in_rat import get_submitted_SHA1, get_clustered_SHA1, \
+    replace_document_with_correct_information, update_operation_entities
+
+
+def calculate_id(rs):
+    h = hashlib.sha1()
+    h.update(str(rs).encode())
+    return h.hexdigest().upper()
+
+
+class TestCorrectAsseblyError(TestCase):
+    def setUp(self) -> None:
+        host = 'localhost'
+        port = 27017
+
+        # Accessioning warehouse
+        self.accession_db = 'eva_accession_sharded'
+        self.submitted_variants_collection = 'submittedVariantEntity'
+        self.clustered_variants_collection = 'clusteredVariantEntity'
+        self.clustered_variants_operation_collection = 'clusteredVariantOperationEntity'
+        self.dbsnp_clustered_variants_collection = 'dbsnpClusteredVariantEntity'
+        uri = f'mongodb://{host}:{port}'
+        self.mongo_db = MongoDatabase(uri=uri, db_name="eva_accession_sharded")
+        self.connection_handle = self.mongo_db.mongo_handle
+
+        # Accessioning db
+        ss1 = {
+            "seq": "GCA_015227675.1",
+            "tax": 1111,
+            "study": "PRJEB30318",
+            "contig": "CM026996.1",
+            "start": 76166296,
+            "ref": "C",
+            "alt": "T",
+            "accession": 5318166021,
+            "rs": 1000,
+        }
+        ss1["_id"] = get_submitted_SHA1(ss1)
+
+        rs1 = {
+            "asm": "GCA_015227675.1",
+            "tax": 1111,
+            "contig": "CM026996.1",
+            "start": 76166296,
+            "accession": 1000,
+            'type': 'SNV'
+        }
+        rs1["_id"] = get_clustered_SHA1(rs1)
+
+        cvoe1 = {
+            "_id": "61a8fe72b0cc848d7a5a194f",
+            "eventType": "MERGED",
+            "accession": 198834536,
+            "mergeInto": 198362526,
+            "reason": "After remapping to GCA_015227675.1, RS IDs mapped to the same locus.",
+            "inactiveObjects": [
+                {
+                        "asm": "GCA_015227675.1",
+                        "tax": 10116,
+                        "contig": "CM026996.1",
+                        "start": 39058078,
+                        "type": "SNV",
+                        "hashedMessage": "AF611B8ED3BE813B427AD7E48D266D3DB5F013B8",
+                        "accession": 198834536,
+                        "version": 1,
+                },
+                {
+                    "asm": "GCA_015227675.1",
+                    "tax": 10116,
+                    "contig": "CM026996.1",
+                    "start": 39058077,
+                    "type": "SNV",
+                    "hashedMessage": "AF611B8ED3BE813B427AD7E48D266D3DB5F013B8",
+                    "accession": 198834532,
+                    "version": 1,
+                }
+            ]
+        }
+
+        self.connection_handle[self.accession_db][self.submitted_variants_collection].drop()
+        self.connection_handle[self.accession_db][self.clustered_variants_collection].drop()
+        self.connection_handle[self.accession_db][self.clustered_variants_operation_collection].drop()
+        self.connection_handle[self.accession_db][self.submitted_variants_collection].insert_many([ss1])
+        self.connection_handle[self.accession_db][self.clustered_variants_collection].insert_many([rs1])
+        self.connection_handle[self.accession_db][self.clustered_variants_operation_collection].insert_many([cvoe1])
+
+    def tearDown(self) -> None:
+        self.connection_handle[self.accession_db][self.submitted_variants_collection].drop()
+        self.connection_handle[self.accession_db][self.clustered_variants_collection].drop()
+        self.connection_handle.close()
+
+    def test_replace_document_with_correct_information(self):
+        replace_document_with_correct_information(
+            self.mongo_db, "submittedVariantEntity", get_submitted_SHA1,
+            {'seq': 'GCA_015227675.1', 'contig': 'CM026996.1'},
+            {'seq': 'GCA_015227675.2', 'contig': 'AY172581.1'},
+            5
+        )
+        cursor = self.connection_handle[self.accession_db][self.submitted_variants_collection].find()
+        document = cursor.__next__()
+        assert document['seq'] == 'GCA_015227675.2'
+        assert document['contig'] == 'AY172581.1'
+
+    def test_update_operation_entities(self):
+        update_operation_entities(
+            self.mongo_db, 'clusteredVariantOperationEntity', get_clustered_SHA1,
+            {'inactiveObjects.asm': 'GCA_015227675.1', 'eventType': 'MERGED'},
+            {'inactiveObjects.asm': 'GCA_015227675.2',
+             'reason': lambda x: x.replace('GCA_015227675.1', 'GCA_015227675.2')},
+            5
+        )
+        cursor = self.connection_handle[self.accession_db][self.clustered_variants_operation_collection].find()
+        document = cursor.__next__()
+        assert document['reason'] == 'After remapping to GCA_015227675.2, RS IDs mapped to the same locus.'
+        assert document['inactiveObjects'][0]['asm'] == 'GCA_015227675.2'
