23d3595176407980ecd92d8357998330023c4eef
April Shen <ashen@ebi.ac.uk>
2022-06-16 13:42:08 +0100

    scripts for remediation of remapped duplicate dbsnp

---
+++ tasks/eva_2862/ExportNewlySplitSS.groovy
@@ -0,0 +1,123 @@
+@Grab(group = 'uk.ac.ebi.eva', module = 'eva-accession-core', version = '0.6.10-SNAPSHOT')
+@Grab(group = 'uk.ac.ebi.eva', module = 'eva-remapping-get-source', version = '0.6.10-SNAPSHOT')
+
+import com.mongodb.MongoCursorNotFoundException
+import htsjdk.variant.variantcontext.VariantContext;
+import org.apache.commons.lang3.tuple.ImmutablePair
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.Import
+import org.springframework.data.domain.Sort
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Component
+import uk.ac.ebi.eva.accession.remapping.source.configuration.batch.io.VariantContextWriterConfiguration
+import uk.ac.ebi.eva.accession.remapping.source.configuration.batch.io.SubmittedVariantsProcessorConfiguration
+import uk.ac.ebi.eva.accession.remapping.source.configuration.InputParametersConfiguration
+import uk.ac.ebi.eva.accession.core.configuration.nonhuman.MongoConfiguration
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantOperationEntity
+
+import static org.springframework.data.mongodb.core.query.Criteria.where
+import static org.springframework.data.mongodb.core.query.Query.query
+
+@Component
+@Import(value=[VariantContextWriterConfiguration.class, SubmittedVariantsProcessorConfiguration.class,
+               InputParametersConfiguration.class, MongoConfiguration.class])
+class ExportNewlySplitSS implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExportNewlySplitSS.class)
+
+    @Autowired
+    private MongoTemplate mongoTemplate
+
+    @Autowired
+    private InputParameters inputParameters
+
+    @Autowired
+    private ItemProcessor<SubmittedVariantEntity, VariantContext> submittedVariantProcessor
+
+    @Autowired
+    private VariantContextWriter variantContextWriter
+
+    void run(String... args) {
+        // Steps:
+        // - read from dbSNP SS operations matching SS_SPLIT_FROM_X_TO_Y
+        // - for each Y, get the EVA SS entities with accession Y
+        // - process and write variant to VCF using existing processor & writer
+
+        int numSplitOperations = 0
+        int batchIndex = 0
+        String lastSeenID = null
+        while (true) {
+            ImmutablePair<List<? extends SubmittedVariantOperationEntity>, String> splitOperationsAndLastSeenID =
+                    getNextBatchOfSplitOperations(lastSeenID)
+            if (splitOperationsAndLastSeenID != null) {
+                List<? extends SubmittedVariantEntity> sves = 
+                        getNextBatchOfSplitEvaSVEs(splitOperationsAndLastSeenID.left)
+                if (sves != null) {
+                    variantContextWriter.write(submittedVariantProcessor.process(sves))
+                }
+                numSplitOperations += splitOperationsAndLastSeenID.left.size()
+                lastSeenID = splitOperationsAndLastSeenID.right
+                logger.info("Processed " + numSplitOperations + " split operations...")
+                batchIndex += 1
+            } else {
+                break
+            }
+        }
+    }
+
+    @Retryable(value = MongoCursorNotFoundException.class, maxAttempts = 5, backoff = @Backoff(delay = 100L))
+    ImmutablePair<List<? extends SubmittedVariantOperationEntity>, String> getNextBatchOfSplitOperations(
+            String lastSeenID) {
+        Query queryToGetNextBatchOfSVO = query(where("_id").regex("^SS_SPLIT_.+")
+                .and("inactiveObjects.seq").is(inputParameters.getAssemblyAccession()))
+        if (Objects.nonNull(lastSeenID)) {
+            queryToGetNextBatchOfSVO.addCriteria(where("_id").gt(lastSeenID))
+        }
+        queryToGetNextBatchOfSVO = queryToGetNextBatchOfSVO.with(Sort.by(Sort.Direction.ASC, "_id")).limit(1000)
+
+        List<? extends SubmittedVariantOperationEntity> result =
+                this.mongoTemplate.find(queryToGetNextBatchOfSVO, DbsnpSubmittedVariantOperationEntity.class)
+        if (result.size() > 0) {
+            return new ImmutablePair<>(result, result.get(result.size() - 1).getId())
+        }
+        return null
+    }
+
+    @Retryable(value = MongoCursorNotFoundException.class, maxAttempts = 5, backoff = @Backoff(delay = 100L))
+    List<? extends SubmittedVariantEntity> getNextBatchOfSplitEvaSVEs(
+            List<? extends SubmittedVariantOperationEntity> operations) {
+        List<Long> accessions = operations.stream()
+                                          .map(SubmittedVariantOperationEntity::getSplitInto)
+                                          .collect(Collectors.toList())
+        Query queryToGetNextBatchOfSVE = query(where("accession").in(accessions)
+                .and("remappedFrom").exists(false))
+        List<? extends SubmittedVariantEntity> result = this.mongoTemplate.find(queryToGetNextBatchOfSVE,
+                SubmittedVariantEntity.class)
+        if (result.size() > 0) {
+            return result
+        }
+        return null
+    }
+
+}
+
+@SpringBootApplication
+class ExportNewlySplitSSApp  {
+
+    static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(ExportNewlySplitSS.class, args)
+        System.exit(SpringApplication.exit(context))
+    }
+
+}
---
+++ tasks/eva_2862/PropagateSplitToRemappedSS.groovy
@@ -0,0 +1,149 @@
+@Grab(group = 'uk.ac.ebi.eva', module = 'eva-accession-clustering', version = '0.6.10-SNAPSHOT')
+@Grab(group = 'uk.ac.ebi.eva', module = 'eva-accession-core', version = '0.6.10-SNAPSHOT')
+@Grab(group = 'uk.ac.ebi.eva', module = 'eva-remapping-ingest', version = '0.6.10-SNAPSHOT')
+
+import com.mongodb.MongoCursorNotFoundException
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.batch.item.ItemReader
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.context.annotation.Import
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Query
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.stereotype.Component
+import uk.ac.ebi.eva.accession.clustering.batch.processors.VariantToSubmittedVariantEntityProcessor
+import uk.ac.ebi.eva.accession.clustering.configuration.batch.processors.CluteringVariantProcessorConfiguration
+import uk.ac.ebi.eva.accession.core.configuration.nonhuman.MongoConfiguration
+import uk.ac.ebi.eva.accession.core.model.dbsnp.DbsnpSubmittedVariantEntity
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantEntity
+import uk.ac.ebi.eva.remapping.ingest.configuration.batch.io.VcfReaderConfiguration
+import uk.ac.ebi.eva.remapping.ingest.configuration.BeanNames;
+import uk.ac.ebi.eva.remapping.ingest.configuration.InputParametersConfiguration
+
+import static org.springframework.data.mongodb.core.query.Criteria.where
+import static org.springframework.data.mongodb.core.query.Query.query
+
+@Component
+@Import(value=[VcfReaderConfiguration.class, CluteringVariantProcessorConfiguration.class,
+               InputParametersConfiguration.class, MongoConfiguration.class])
+class PropagateSplitToRemappedSS implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(PropagateSplitToRemappedSS.class)
+
+    @Autowired
+    private MongoTemplate mongoTemplate
+
+    @Autowired
+    private InputParameters inputParameters
+
+    @Autowired
+    @Qualifier(BeanNames.VCF_READER)
+    private ItemReader<Variant> vcfReader
+
+    @Autowired
+    private VariantToSubmittedVariantEntityProcessor variantProcessor
+    
+
+    void run(String... args) {
+        // Steps:
+        // - read from remapped vcf
+        // - for each variant, compute the hash and get the corresponding document from dbSNP SVE
+        // - update the SS while preserving everything else
+        // - insert to EVA SVE
+        // - delete originals from dbSNP SVE
+
+        int chunkSize = 1000
+        int count = 0
+        List<SubmittedVariantEntity> chunk = new ArrayList<>()
+
+        Variant variant = vcfReader.read()
+
+        while (variant != null) {
+            SubmittedVariantEntity sve = variantProcessor.process(variant)
+
+            if (count < chunkSize) {
+                chunk.add(sve)
+                count++
+            } else {
+                processChunk(chunk)
+                count = 0
+                chunk = new ArrayList<>()
+            }
+            variant = vcfReader.read()
+        }
+
+        // process the last chunk, if it exists
+        if (!chunk.isEmpty()) {
+            processChunk(chunk)
+        }
+    }
+
+    void processChunk(List<? extends SubmittedVariantEntity> chunk) {
+        List<? extends SubmittedVariantEntity> remappedSVEWithOldSS = getSVEWithSameHash(chunk)
+        if (remappedSVEWithOldSS != null) {
+            List<SubmittedVariantEntity> remappedSVEWithUpdatedSS = getUpdatedSVE(remappedSVEWithOldSS, sve)
+            mongoTemplate.insert(remappedSVEWithUpdatedSS, SubmittedVariantEntity.class)
+            mongoTemplate.findAndRemove(remappedSVEWithOldSS, DbsnpSubmittedVariantEntity.class)
+        }
+}
+
+    @Retryable(value = MongoCursorNotFoundException.class, maxAttempts = 5, backoff = @Backoff(delay = 100L))
+    List<? extends SubmittedVariantEntity> getSVEWithSameHash(List<? extends SubmittedVariantEntity> sves) {
+        List<String> hashes = sves.stream()
+                                  .map(SubmittedVariantEntity::getHashedMessage)
+                                  .collect(Collectors.toList())
+        Query queryToGetNextBatchOfSVE = query(where("_id").in(hashes))
+        List<? extends SubmittedVariantEntity> result = this.mongoTemplate.find(queryToGetNextBatchOfSVE,
+                DbsnpSubmittedVariantEntity.class)
+        if (result.size() > 0) {
+            return result
+        }
+        return null
+    }
+
+    List<? extends SubmittedVariantEntity> getUpdatedSVE(List<? extends SubmittedVariantEntity> sveWithOldSS,
+                                                         SubmittedVariantEntity sveWithNewSS) {
+        Long newSS = sveWithNewSS.getAccession()
+        List<SubmittedVariantEntity> updatedSVE = new ArrayList<>()
+        for (SubmittedVariantEntity sve : sveWithOldSS) {
+            SubmittedVariant variant = new SubmittedVariant(
+                sve.getReferenceSequenceAccession(),
+                sve.getTaxonomyAccession(),
+                sve.getProjectAccession(),
+                sve.getContig(),
+                sve.getStart(),
+                sve.getReferenceAllele(),
+                sve.getAlternateAllele(),
+                sve.getClusteredVariantAccession())  // preserve RSID from the old SVE
+            updatedSVE.add(new SubmittedVariantEntity(
+                newSS, 
+                sve.getHashedMessage(),
+                variant,
+                sve.getVersion(),
+                sve.getRemappedFrom(), 
+                sve.getRemappedDate(),
+                sve.getRemappingId()))
+        }
+
+        return updatedSVE
+    }
+
+}
+
+
+@SpringBootApplication
+class PropagateSplitToRemappedSSApp  {
+
+    static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(PropagateSplitToRemappedSS.class, args)
+        System.exit(SpringApplication.exit(context))
+    }
+
+}
