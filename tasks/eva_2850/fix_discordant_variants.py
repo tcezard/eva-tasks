8a4d38e38021b84dd6fa3cb0e24f9c40076527c4
nkumar2 <nkumar2@ebi.ac.uk>
2022-07-01 18:22:34 +0100

    fix discordant variants

--- tasks/eva_2850/fix_discordant_variants.py
+++ tasks/eva_2850/fix_discordant_variants.py
@@ -51,7 +51,7 @@ def fix_discordant_variants(mongo_source, assembly, rs_file, batch_size=1000):
             logger.info(f"Processing Batch. Num_of_RS in batch : {len(rs_list)} \nRS_list -> {rs_list}")
 
             dbsnp_rs_variants, eva_rs_variants, all_rs_variants = get_rs_variants(mongo_source, assembly, rs_list)
-            dbsnp_ss_variants, eva_ss_variants, all_ss_variants = get_ss_variants(mongo_source, rs_list)
+            dbsnp_ss_variants, eva_ss_variants, all_ss_variants = get_ss_variants(mongo_source, assembly, rs_list)
             dbsnp_events, eva_events, all_events = get_rs_events(mongo_source, rs_list)
 
             for rs in rs_list:
@@ -59,46 +59,64 @@ def fix_discordant_variants(mongo_source, assembly, rs_file, batch_size=1000):
 
                 if rs not in all_rs_variants:
                     logger.error(f"No RS variant could be found for RS {rs}")
+                    rs_events = all_events[rs]
+                    for event in rs_events:
+                        if event['accession'] == rs and event['eventType'] == 'MERGED':
+                            merge_into_RS = event['mergeInto']
+                            logger.error(
+                                f"RS {rs} has been merged into RS {merge_into_RS}. Adding {merge_into_RS} to the list")
+                            rs_list.append(rs)
+                            break
                     continue
                 else:
-                    rs_without_map_weight = get_rs_without_map_weight(all_rs_variants[rs])
+                    rs_records = all_rs_variants[rs]
+
+                    rs_without_map_weight = get_rs_without_map_weight(rs_records)
                     if not rs_without_map_weight:
-                        logger.error(f"All variants for {rs} are map-weighted : \n{all_rs_variants[rs]}")
+                        logger.error(f"All variants for {rs} are map-weighted : \n{rs_records}")
                         continue
                     elif len(rs_without_map_weight) > 1:
                         logger.error(f"More than one variant without map-weight found for RS {rs} :"
-                                     f"\n {all_rs_variants[rs]}")
+                                     f"\n {rs_without_map_weight}")
                         continue
 
-                    rs_variant = rs_without_map_weight[0]
-                    non_remapped_ss = [ss for ss in all_ss_variants[rs] if
-                                       'remappedFrom' not in ss and ss['seq'] == rs_variant['asm']]
-
-                    if not non_remapped_ss:
-                        logger.error(f"No original SS found for RS {rs}, /n {all_ss_variants[rs]}")
+                    ss_records = all_ss_variants[rs]
+                    if not ss_records:
+                        logger.error(f"No original SS record found for RS {rs}, /n {ss_records}")
                         continue
 
-                    if len(non_remapped_ss) > 1:
-                        if not check_all_ss_has_same_info(non_remapped_ss):
-                            logger.error(f"Not all original SS has same info : \n {non_remapped_ss}")
+                    rs_variant = rs_records[0]
+
+                    if check_all_ss_has_same_info(ss_records):
+                        if rs_variant['start'] == ss_records[0]['start']:
+                            logger.error(f"RS's and original SS's Start matches. Nothing to do")
                             continue
 
-                    if rs_variant['start'] == non_remapped_ss[0]['start']:
-                        logger.error(f"RS's and original SS's Start matches. Nothing to do")
-                        continue
+                        process_all_ss_has_same_info(rs_variant, ss_records)
 
-                    rs_with_new_start = copy.copy(rs_variant)
-                    rs_with_new_start['start'] = non_remapped_ss[0]['start']
-                    rs_with_new_start['_id'] = get_clustered_SHA1(rs_with_new_start)
-
-                    result, variant_in_db = check_for_hash_collision(rs_with_new_start['_id'])
-
-                    if result:
-                        logger.warn(
-                            f"Hash collision will occur for RS {rs_variant} with RS {variant_in_db['accession']}")
-                        resolve_collision_and_insert_rs(rs_variant, rs_with_new_start, variant_in_db)
                     else:
-                        correct_rs_start_and_insert(rs_variant, rs_with_new_start)
+                        logger.error(f"Not all original SS has same info : \n {ss_records}")
+                        process_ss_diff_issue_rs_split(rs_variant, ss_records)
+
+
+def process_ss_diff_issue_rs_split(rs_variant, ss_records):
+    # TODO : Split RS for discordant SS variants
+    pass
+
+
+def process_all_ss_has_same_info(rs_variant, ss_records):
+    rs_with_new_start = copy.copy(rs_variant)
+    rs_with_new_start['start'] = ss_records[0]['start']
+    rs_with_new_start['_id'] = get_clustered_SHA1(rs_with_new_start)
+
+    result, variant_in_db = check_for_hash_collision(rs_with_new_start['_id'])
+
+    if result:
+        logger.warn(f"Hash collision will occur for RS {rs_variant['accession']} with RS {variant_in_db['accession']}")
+        resolve_collision_and_insert_rs(rs_variant, rs_with_new_start, variant_in_db)
+    else:
+        logger.info(f"No hash collision for RS {rs_variant['accession']}")
+        correct_rs_start_and_insert(rs_variant, rs_with_new_start)
 
 
 def correct_rs_start_and_insert(rs_variant, rs_with_new_start):
@@ -109,10 +127,10 @@ def correct_rs_start_and_insert(rs_variant, rs_with_new_start):
     drop_statements.append(pymongo.DeleteOne({'_id': rs_variant['_id']}))
     insert_statements.append(pymongo.InsertOne(rs_with_new_start))
 
-    result_drop = dbsnp_cve_collection.bulk_write(requests=drop_statements, ordered=False)
-    logger.info('There was %s old documents dropped' % result_drop.deleted_count)
-    result_insert = dbsnp_cve_collection.bulk_write(requests=insert_statements, ordered=False)
-    logger.info('There was %s new documents inserted' % result_insert.inserted_count)
+    # result_drop = dbsnp_cve_collection.bulk_write(requests=drop_statements, ordered=False)
+    # logger.info('There was %s old documents dropped' % result_drop.deleted_count)
+    # result_insert = dbsnp_cve_collection.bulk_write(requests=insert_statements, ordered=False)
+    # logger.info('There was %s new documents inserted' % result_insert.inserted_count)
 
 
 def resolve_collision_and_insert_rs(rs_variant, rs_with_new_start, variant_in_db):
@@ -131,20 +149,20 @@ def resolve_collision_and_insert_rs(rs_variant, rs_with_new_start, variant_in_db
         merge_event = create_merge_event(variant_in_db, rs_with_new_start)
         dbsnp_cvoe_insert_statements.append(pymongo.InsertOne(merge_event))
 
-        result_drop = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_drop_statements, ordered=False)
-        logger.info('There was %s old documents dropped' % result_drop.deleted_count)
-        result_cve_insert = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_insert_statements, ordered=False)
-        logger.info('There was %s new documents inserted' % result_cve_insert.inserted_count)
-        result_cvoe_insert = dbsnp_cvoe_collection.bulk_write(requests=dbsnp_cvoe_insert_statements, ordered=False)
-        logger.info('There was %s new documents inserted' % result_cvoe_insert.inserted_count)
+        # result_drop = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_drop_statements, ordered=False)
+        # logger.info('There was %s old documents dropped' % result_drop.deleted_count)
+        # result_cve_insert = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_insert_statements, ordered=False)
+        # logger.info('There was %s new documents inserted' % result_cve_insert.inserted_count)
+        # result_cvoe_insert = dbsnp_cvoe_collection.bulk_write(requests=dbsnp_cvoe_insert_statements, ordered=False)
+        # logger.info('There was %s new documents inserted' % result_cvoe_insert.inserted_count)
 
         update_ss_with_new_rs(variant_in_db['accession'], rs_with_new_start['accession'])
 
     else:
         dbsnp_cve_drop_statements.append(pymongo.DeleteOne({'_id': rs_variant['_id']}))
 
-        result_drop = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_drop_statements, ordered=False)
-        logger.info('There was %s old documents dropped' % result_drop.deleted_count)
+        # result_drop = dbsnp_cve_collection.bulk_write(requests=dbsnp_cve_drop_statements, ordered=False)
+        # logger.info('There was %s old documents dropped' % result_drop.deleted_count)
 
         update_ss_with_new_rs(rs_with_new_start['accession'], variant_in_db['accession'])
 
@@ -168,8 +186,10 @@ def update_ss_with_new_rs(old_rs, new_rs):
     filter_query = {'rs': old_rs}
     update_value = {'$set': {'rs': new_rs}}
 
-    dbsnp_sve_collection.update_many(filter_query, update_value)
-    eva_sve_collection.update_many(filter_query, update_value)
+    # dbsnp_result_updated = dbsnp_sve_collection.update_many(filter_query, update_value)
+    # logger.info('There was %s documents dbsnp updated' % dbsnp_result_updated.modified_count)
+    # eva_result_updated = eva_sve_collection.update_many(filter_query, update_value)
+    # logger.info('There was %s documents eva updated' % eva_result_updated.modified_count)
 
 
 def get_rs_variants(mongo_source, assembly, rs_list):
@@ -180,8 +200,8 @@ def get_rs_variants(mongo_source, assembly, rs_list):
     return dbsnp_rs_variants, eva_rs_variants, all_rs_variants
 
 
-def get_ss_variants(mongo_source, rs_list):
-    ss_filter_criteria = {'rs': {'$in': rs_list}}
+def get_ss_variants(mongo_source, assembly, rs_list):
+    ss_filter_criteria = {'seq': assembly, 'rs': {'$in': rs_list}}
     dbsnp_ss_variants = get_variants(mongo_source, DBSNP_SUBMITTED_VARIANT_ENTITY, ss_filter_criteria, 'rs')
     eva_ss_variants = get_variants(mongo_source, EVA_SUBMITTED_VARIANT_ENTITY, ss_filter_criteria, 'rs')
     all_ss_variants = merge_all_records(dbsnp_ss_variants, eva_ss_variants)
@@ -206,13 +226,13 @@ def get_events(mongo_source, collection_name, filter_criteria):
                 records[event['accession']].append(event)
             else:
                 records[event['accession']] = [event]
-            if event['mergeInto'] in records:
+            if 'mergeInto' in event and event['mergeInto'] in records:
                 records[event['mergeInto']].append(event)
-            else:
+            elif 'mergeInto' in event and event['mergeInto'] not in records:
                 records[event['mergeInto']] = [event]
-            if event['splitInto'] in records:
+            if 'splitInto' in event and event['splitInto'] in records:
                 records[event['splitInto']].append(event)
-            else:
+            elif 'splitInto' in event and event['splitInto'] not in records:
                 records[event['splitInto']] = [event]
     except Exception as e:
         print(traceback.format_exc())
@@ -289,24 +309,12 @@ def check_all_ss_has_same_info(ss_list):
 
 def check_for_hash_collision(id):
     hash_collision_filter_criteria = {'_id': id}
-    collision_records = find_documents(mongo_source, 'dbsnpClusteredVariantEntity', hash_collision_filter_criteria)
+    collision_records = find_documents(mongo_source, DBSNP_CLUSTERED_VARIANT_ENTITY, hash_collision_filter_criteria)
 
     if collision_records:
         return True, collision_records[0]
     else:
         return False, None
-
-
-# def check_for_hash_collision(id_list):
-#     hash_collision_filter_criteria = {'_id': {'$in': [x for x in id_list.keys()]}}
-#     collision_records = find_documents(mongo_source, 'dbsnpClusteredVariantEntity', hash_collision_filter_criteria)
-#
-#     if len(collision_records) > 0:
-#         for record in collision_records:
-#             if record['_id'] in id_list:
-#                 rs = id_list[record['_id']]['accession']
-#                 logger.error(f"Hash Collision will occur for RS : {rs} with {record['accession']} : "
-#                              f"\nRS:       {id_list[record['_id']]} \nRS in DB: {record}")
 
 
 if __name__ == "__main__":
