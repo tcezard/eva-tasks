f266e1c96fa592dbc1c592304d332ad943d4871e
nkumar2 <nkumar2@ebi.ac.uk>
2022-09-19 20:34:35 +0100

    correct db name for prod

--- tasks/eva_2850/fix_discordant_variants.py
+++ tasks/eva_2850/fix_discordant_variants.py
@@ -404,7 +404,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     mongo_source = MongoDatabase(uri=args.mongo_source_uri, secrets_file=args.mongo_source_secrets_file,
-                                 db_name="eva2959_accession_sharded")
+                                 db_name="eva_accession_sharded")
 
     # Sometimes the cluster creates hidden files like .nfs<numeric ID> in the folder when a file is being read. So we just want to focus on the ones that start with GCA
     all_files = [os.path.join(args.discordant_rs_dir, filename) for filename in os.listdir(args.discordant_rs_dir) if
