ddef67226aad505a8118e00ac91f5b689026d773
tcezard <tcezard@ebi.ac.uk>
2022-11-25 09:19:45 +0000

    Fix removal of the scratch dir and address review comments

--- tasks/eva_3090/archive_to_lts.py
+++ tasks/eva_3090/archive_to_lts.py
@@ -12,19 +12,23 @@ logger = logging_config.get_logger(__name__)
 
 
 def make_tarfile(output_filename, source_dir):
+    logger.info(f'Create Final Tar file {output_filename}.')
     with tarfile.open(output_filename, "w") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
+    file_stats = os.stat(output_filename)
+    logger.info(f'{output_filename} completed. File Size in Bytes is {file_stats.st_size}')
 
 
 def is_compressed(file_path):
     compressed_ext = ['.gz', '.zip', '.bz', '.tbi', '.csi']
     for ext in compressed_ext:
-        if file_path.endswith(ext):
+        if file_path.lower().endswith(ext):
             return True
     return False
 
 
-def compress(src_file_path, dest_file_path):
+@retry(tries=5, delay=3, backoff=2, logger=logger)
+def retriable_compress(src_file_path, dest_file_path):
     MEG = 2 ** 20
     with open(src_file_path, 'rb') as f_in:
         with gzip.open(dest_file_path, 'wb') as f_out:
@@ -40,12 +44,20 @@ def retriable_remove(path):
     shutil.rmtree(path)
 
 
+@retry(tries=5, delay=3, backoff=2, logger=logger)
+def retryable_copy(src_file_path, dest_file_path, **kwargs):
+    if os.path.exists(dest_file_path):
+        os.remove(dest_file_path)
+    shutil.copyfile(src_file_path, dest_file_path, **kwargs)
+
+
 def archive_directory(root_dir, scratch_dir, destination_dir, filter_patterns=None):
 
     root_dir_name = os.path.basename(root_dir)
     logger.info(f'Archive {root_dir_name} from {root_dir}')
     parent_root_dir = os.path.dirname(root_dir)
     for base, dirs, files in os.walk(root_dir, topdown=True, followlinks=False):
+        # Filter the downstream directory to
         filtered_dir = []
         for d in dirs:
             if matches(d, filter_patterns):
@@ -56,32 +68,25 @@ def archive_directory(root_dir, scratch_dir, destination_dir, filter_patterns=No
         dirs[:] = filtered_dir
         src_basename = os.path.relpath(base, parent_root_dir)
         scratch_dest_dir = os.path.join(scratch_dir, src_basename)
-        if matches(src_basename, filter_patterns):
-
-            continue
         os.makedirs(scratch_dest_dir, exist_ok=True)
         for fname in files:
             src_file_path = os.path.join(base, fname)
             dest_file_path = os.path.join(scratch_dest_dir, fname)
             if matches(fname, filter_patterns):
-                logger.info(f'Ignore file f{src_file_path} because of filters: {filter_patterns}')
+                logger.info(f'Ignore file {src_file_path} because of filters: {filter_patterns}')
                 continue
-            if os.path.islink(src_file_path):
-                if os.path.exists(dest_file_path):
-                    os.remove(dest_file_path)
-                shutil.copyfile(src_file_path, dest_file_path, follow_symlinks=False)
-            elif is_compressed(src_file_path):
+            if os.path.islink(src_file_path) or is_compressed(src_file_path):
                 logger.info(f'Copy {src_file_path}')
-                if os.path.exists(dest_file_path):
-                    os.remove(dest_file_path)
-                shutil.copyfile(src_file_path, dest_file_path)
+                retryable_copy(src_file_path, dest_file_path, follow_symlinks=False)
             else:
                 logger.info(f'Compress {src_file_path}')
-                compress(src_file_path, dest_file_path + '.gz')
+                retriable_compress(src_file_path, dest_file_path + '.gz')
     final_tar_file = os.path.join(destination_dir, root_dir_name + '.tar')
-    logger.info(f'Create Final Tar file {final_tar_file}')
-    make_tarfile(final_tar_file, os.path.join(scratch_dir, root_dir_name))
-    retriable_remove(scratch_dir)
+    scratch_dir_archived = os.path.join(scratch_dir, root_dir_name)
+    make_tarfile(final_tar_file, scratch_dir_archived)
+    logger.info(f'Delete scratch folder {scratch_dir_archived}.')
+    retriable_remove(scratch_dir_archived)
+    logger.info(f'Scratch folder {scratch_dir_archived} deleted.')
 
 
 def main():
