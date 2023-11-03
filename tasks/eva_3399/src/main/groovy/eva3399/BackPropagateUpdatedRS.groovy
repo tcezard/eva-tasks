9197fe59ec9324313a45e41ff94bd028c1465153
sundarvenkata-ebi <sundarvenkata@ebi.ac.uk>
2023-11-03 09:19:29 +0000

    Add SS merge targets as another set of SS to be back-propped

--- tasks/eva_3399/src/main/groovy/eva3399/BackPropagateUpdatedRS.groovy
+++ tasks/eva_3399/src/main/groovy/eva3399/BackPropagateUpdatedRS.groovy
@@ -3,6 +3,7 @@ package eva3399
 import org.springframework.data.mongodb.core.query.Update
 import uk.ac.ebi.eva.accession.clustering.batch.io.BackPropagatedRSWriter
 import uk.ac.ebi.eva.accession.clustering.configuration.BeanNames
+import uk.ac.ebi.eva.accession.core.model.eva.SubmittedVariantOperationEntity
 import uk.ac.ebi.eva.groovy.commons.EVADatabaseEnvironment
 import uk.ac.ebi.eva.groovy.commons.RetryableBatchingCursor
 
@@ -25,11 +26,19 @@ class BackPropagateUpdatedRS {
         def backPropagatedRSWriter =
                 this.backPropEnv.springApplicationContext.getBean(BeanNames.BACK_PROPAGATED_RS_WRITER,
                         BackPropagatedRSWriter.class)
-        def impactedSS = [sveClass, dbsnpSveClass].collect{new RetryableBatchingCursor<>(
-                where("seq").is(this.remappedAssembly).and("remappedFrom").is(this.sourceAssembly)
-                        .and("rs").exists(true), this.backPropEnv.mongoTemplate, it)}
-        impactedSS.each{it.each{ssInBatch ->
-            def impactedSSIds = ssInBatch.collect{it.accession}.toSet()
+        def opsOfImpactedSS = [svoeClass, dbsnpSvoeClass].collect{
+            [new RetryableBatchingCursor<>(
+                where("_id").regex("^EVA3399_UPD_LOCUS_${this.remappedAssembly}_.*").and(
+                        "inactiveObjects.remappedFrom").is(this.sourceAssembly),
+                this.backPropEnv.mongoTemplate, it),
+             new RetryableBatchingCursor<>(
+                    where("_id").regex("^EVA3399_MERGED_${this.remappedAssembly}_.*").and(
+                            "inactiveObjects.remappedFrom").is(this.sourceAssembly),
+                    this.backPropEnv.mongoTemplate, it)]
+        }
+        opsOfImpactedSS.each{it.each{ it.each{List<SubmittedVariantOperationEntity> opsInBatch ->
+            def impactedSSIds = opsInBatch.collect{it.accession}.toSet()
+            impactedSSIds.addAll(opsInBatch.findAll{Objects.nonNull(it.mergedInto)}.collect{it.mergedInto}.toSet())
             [sveClass, dbsnpSveClass].each{ssClass ->
                 this.backPropEnv.mongoTemplate.updateMulti(query(where("seq").is(this.sourceAssembly)
                         .and("accession").in(impactedSSIds)), new Update().unset("backPropRS"), ssClass)
@@ -39,6 +48,6 @@ class BackPropagateUpdatedRS {
                         .and("accession").in(impactedSSIds)), ssClass)
             }.flatten()
             backPropagatedRSWriter.write(changedSS)
-        }}
+        }}}
     }
 }
