ddf9ad18875f89c20114fa322ffacaecf80c1339
tcezard <tcezard@ebi.ac.uk>
2024-01-12 22:56:15 +0000

    Batch search query as well as query to write the candidates

--- tasks/eva_3450/src/main/groovy/eva3450/CreateMergeCandidates.groovy
+++ tasks/eva_3450/src/main/groovy/eva3450/CreateMergeCandidates.groovy
@@ -34,29 +34,73 @@ class CreateMergeCandidates {
         this.batchSize = batchSize
     }
 
+
     def process = {
         def is = new File(mergeCandidatesFile).newInputStream()
         logger.info("Create Merge candidate documents in batches of ${batchSize}")
         int totalInsertedCount = 0
         int lineRead = 0
         def mergedCandidateOperations = []
+        ArrayList<String> batchedLines = new ArrayList<String>()
         is.eachLine{
             lineRead = lineRead + 1
+            batchedLines.add(it)
+            if (batchedLines.size() == batchSize){
+                def mergeCandidateOperations = createBatchMergeCandidates(batchedLines)
+                totalInsertedCount = insertOperations(mergeCandidateOperations, totalInsertedCount, lineRead)
+                batchedLines = new ArrayList()
+            }
+        }
+        if (batchedLines.size() > 0){
+            def mergeCandidateOperations = createBatchMergeCandidates(batchedLines)
+            totalInsertedCount = insertOperations(mergeCandidateOperations, totalInsertedCount, lineRead)
+        }
+        is.close()
+
+    }
+
+    def createBatchMergeCandidates = { List<String> hashAndRsids ->
+        Map<String, Long[]> hashToRsids = hashAndRsids.collectEntries {
             String[] sp_line =  it.split("\\t")
             def hash = sp_line.first()
             def rsids = sp_line.tail().collect({it.toLong()})
-            mergedCandidateOperations.add(createSingleMergeCandidate(hash, rsids))
-            if (mergedCandidateOperations.size() == batchSize){
-                totalInsertedCount = insertOperations(mergedCandidateOperations, totalInsertedCount, lineRead)
-                mergedCandidateOperations = [];
+            return [ (hash): rsids]
+        }
+        def rsids = hashToRsids.values().toList().flatten()
+        def evaAndDbsnpSveCursors = [sveClass, dbsnpSveClass].collect { collectionClass ->
+            prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").in(rsids)), collectionClass)
+        }
+        def allSves = evaAndDbsnpSveCursors.each { cursor ->
+            cursor.each { sves -> sves}}.flatten()
+        Map<String, SubmittedVariantEntity[]> hashToSves = new HashMap<>()
+        allSves.each {SubmittedVariantEntity sve ->
+            def hash = EVAObjectModelUtils.getClusteredVariantHash(sve)
+            if (! hashToSves.containsKey(hash) ){
+                hashToSves.put(hash, new ArrayList<SubmittedVariantEntity>())
             }
+            hashToSves[hash].add(sve)
         }
-
-        if (mergedCandidateOperations.size() > 0){
-            totalInsertedCount = insertOperations(mergedCandidateOperations, totalInsertedCount, lineRead)
-            mergedCandidateOperations = [];
+        def mergeCandidateOperations= hashToSves.keySet().collect {String hash ->
+            allSves = hashToSves.get(hash)
+            rsids = allSves.collect(sve -> sve.clusteredVariantAccession).toSet().toList()
+            if (! isMergeIsValid(allSves, hash, rsids)) {
+                return null
+            }
+            // create MERGE_CANDIDATE operation
+            def submittedVariantInactiveEntity = allSves.collect {new SubmittedVariantInactiveEntity(it)}
+            SubmittedVariantOperationEntity svoe = new SubmittedVariantOperationEntity()
+            Long rsAccession = ((SubmittedVariantEntity)allSves[0]).clusteredVariantAccession
+            svoe.fill(
+                    EventType.RS_MERGE_CANDIDATES, rsAccession,
+                    "RS mismatch with " + rsAccession, submittedVariantInactiveEntity
+            )
+            svoe.setId(String.format("%s_%s_%s", RSMergeAndSplitCandidatesReaderConfiguration.MERGE_CANDIDATE_ID_PREFIX,
+                                     assembly, hash))
+            return svoe
         }
-        is.close()
+        // Remove null for invalid MERGE candidates
+        mergeCandidateOperations.removeAll([null])
+        return mergeCandidateOperations
     }
 
     def insertOperations = {ArrayList mergedCandidateOperations, int totalInsertedCount, int lineRead ->
@@ -66,32 +110,18 @@ class CreateMergeCandidates {
         return totalInsertedCount
     }
 
-    def createSingleMergeCandidate = { String hash, List<Long> rsids ->
-        def evaAndDbsnpSveCursors = [sveClass, dbsnpSveClass].collect { collectionClass ->
-            prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").in(rsids)), collectionClass)
-        }
-        def allSves = evaAndDbsnpSveCursors.each { cursor ->
-            cursor.each { sves -> sves}}.flatten()
-        def hashSet = allSves.collect { SubmittedVariantEntity sve -> EVAObjectModelUtils.getClusteredVariantHash(sve) }.toSet()
-        assertMergeIsValid(allSves, hash)
-        // create MERGED_CANDIDATE operation
-        def submittedVariantInactiveEntity = allSves.collect {new SubmittedVariantInactiveEntity(it)}
-        SubmittedVariantOperationEntity svoe = new SubmittedVariantOperationEntity()
-        Long rsAccession = ((SubmittedVariantEntity)allSves[0]).clusteredVariantAccession
-        svoe.fill(
-                EventType.RS_MERGE_CANDIDATES, rsAccession,
-                "RS mismatch with " + rsAccession, submittedVariantInactiveEntity
-        )
-        svoe.setId(String.format("%s_%s_%s", RSMergeAndSplitCandidatesReaderConfiguration.MERGE_CANDIDATE_ID_PREFIX,
-                                 assembly, hash))
-        return svoe
-    }
-
-    def assertMergeIsValid = { ArrayList<SubmittedVariantEntity> allSves, String hash ->
+    def isMergeIsValid = { ArrayList<SubmittedVariantEntity> allSves, String hash, ArrayList<Long> rsids->
         //Check that all the sve provided have the same clustered variant hash which match the one from the input
         def hashSet = allSves.collect { sve -> EVAObjectModelUtils.getClusteredVariantHash(sve) }.toSet()
-        assert hashSet.size() == 1
-        assert hashSet.head() == hash
+        if (hashSet.size() == 1 && hashSet.head() == hash){
+            return true
+        }else if (hashSet.size() > 1) {
+            logger.warn("More than one Hash in associated with the group of rsids ${rsids}")
+            return false
+        }else{
+            logger.warn("Hash from file ${hash} is different from Hash from the one in the one in database ${hashSet.head()}")
+            return false
+        }
     }
 
 
--- tasks/eva_3450/src/main/groovy/eva3450/CreateSplitCandidates.groovy
+++ tasks/eva_3450/src/main/groovy/eva3450/CreateSplitCandidates.groovy
@@ -1,6 +1,5 @@
 package eva3450
 
-import com.google.common.collect.Iterables
 import org.slf4j.LoggerFactory
 import uk.ac.ebi.ampt2d.commons.accession.core.models.EventType
 import uk.ac.ebi.eva.accession.clustering.configuration.batch.io.RSMergeAndSplitCandidatesReaderConfiguration
@@ -35,31 +34,31 @@ class CreateSplitCandidates {
         this.batchSize = batchSize
     }
 
+
     def process = {
         def is = new File(splitCandidatesFile).newInputStream()
         logger.info("Create Split candidate documents in batches of ${batchSize}")
         int totalInsertedCount = 0
         int lineRead = 0
         def splitCandidateOperations = []
-
+        ArrayList<String> batchedLines = new ArrayList<String>()
         is.eachLine{
             lineRead = lineRead + 1
-            String[] sp_line = it.split("\\t")
-            def rsid = sp_line.first().toLong()
-            def hashes = sp_line.tail()
-            splitCandidateOperations.add(createSingleSplitCandidate(rsid, hashes))
-            if (splitCandidateOperations.size() == batchSize){
+            batchedLines.add(it)
+            if (batchedLines.size() == batchSize){
+                splitCandidateOperations = createBatchSplitCandidates(batchedLines)
                 totalInsertedCount = insertOperations(splitCandidateOperations, totalInsertedCount, lineRead)
-                splitCandidateOperations = [];
+                batchedLines = new ArrayList()
             }
         }
-        if (splitCandidateOperations.size() > 0){
+        if (batchedLines.size() > 0){
+            splitCandidateOperations = createBatchSplitCandidates(batchedLines)
             totalInsertedCount = insertOperations(splitCandidateOperations, totalInsertedCount, lineRead)
-            splitCandidateOperations = [];
         }
         is.close()
 
     }
+
     def insertOperations = {ArrayList splitCandidateOperations, int totalInsertedCount, int lineRead ->
         def insertedCount = devEnv.bulkInsertIgnoreDuplicates(splitCandidateOperations, SubmittedVariantOperationEntity.class)
         totalInsertedCount = totalInsertedCount + insertedCount
@@ -67,29 +66,56 @@ class CreateSplitCandidates {
         return totalInsertedCount
     }
 
-    def createSingleSplitCandidate = { Long rsid, String[] hashes ->
+    def createBatchSplitCandidates = { List<String> rsidAndHashes ->
+        Map<Long, String[]> rsidsToHashes = rsidAndHashes.collectEntries {
+            String[] sp_line = it.split("\\t")
+            def rsid = sp_line.first().toLong()
+            def hashes = sp_line.tail()
+            return [ (rsid): hashes]
+        }
+        def rsids = rsidsToHashes.keySet().toList()
         def evaAndDbsnpSveCursors = [sveClass, dbsnpSveClass].collect { collectionClass ->
-            prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").is(rsid)), collectionClass)
+            prodEnv.mongoTemplate.find(query(where("seq").is(assembly).and("rs").in(rsids)), collectionClass)
         }
         def allSves = evaAndDbsnpSveCursors.each { cursor ->
             cursor.each { sves -> sves}}.flatten()
-        assertSplitIsValid(allSves, hashes)
-        // create SPLIT_CANDIDATE operation
-        def submittedVariantInactiveEntity = allSves.collect {new SubmittedVariantInactiveEntity(it)}
-        SubmittedVariantOperationEntity svoe = new SubmittedVariantOperationEntity()
-        svoe.fill(
-                EventType.RS_SPLIT_CANDIDATES, rsid,
-                "Hash mismatch with " + rsid, submittedVariantInactiveEntity
-        )
-        svoe.setId(String.format("%s_%s_%s", RSMergeAndSplitCandidatesReaderConfiguration.SPLIT_CANDIDATE_ID_PREFIX,
-                                 assembly, rsid))
-        return svoe
+        Map<Long, SubmittedVariantEntity[]> rsidToSves = new HashMap<>()
+        allSves.each {SubmittedVariantEntity sve ->
+            if (! rsidToSves.containsKey(sve.clusteredVariantAccession) ){
+                rsidToSves.put(sve.clusteredVariantAccession, new ArrayList<SubmittedVariantEntity>())
+            }
+            rsidToSves[sve.clusteredVariantAccession].add(sve)
+        }
+        def splitCandidateOperations= rsids.collect {Long rsid ->
+            def sves = rsidToSves.get(rsid)
+            def hashes = rsidsToHashes.get(rsid)
+            if (! isSplitIsValid(sves, hashes, rsid)) {
+                return null
+            }
+            // create SPLIT_CANDIDATE operation
+            def submittedVariantInactiveEntity = allSves.collect {new SubmittedVariantInactiveEntity(it)}
+            SubmittedVariantOperationEntity svoe = new SubmittedVariantOperationEntity()
+            svoe.fill(
+                    EventType.RS_SPLIT_CANDIDATES, rsid,
+                    "Hash mismatch with " + rsid, submittedVariantInactiveEntity
+            )
+            svoe.setId(String.format("%s_%s_%s", RSMergeAndSplitCandidatesReaderConfiguration.SPLIT_CANDIDATE_ID_PREFIX,
+                    assembly, rsid))
+            return svoe
+        }
+        // Remove null for invalid SPLIT candidates
+        splitCandidateOperations.removeAll([null])
+        return splitCandidateOperations
     }
 
-    def assertSplitIsValid = { ArrayList<SubmittedVariantEntity> allSves, String[] hashes ->
+    def isSplitIsValid = { ArrayList<SubmittedVariantEntity> allSves, String[] hashes, Long rsid ->
         //Check that all the sve provided have the same clustered variant hash which match the one from the input
         def hashSet = allSves.collect { sve -> EVAObjectModelUtils.getClusteredVariantHash(sve) }.toSet()
-        assert hashSet == hashes.toList().toSet()
+        if (hashSet == hashes.toList().toSet()){
+            return true
+        } else {
+            logger.warn("Hash from files ${hashSet} is different from Hash from rs${rsid} : ${hashes.toList().toSet()}")
+            return false
+        }
     }
-
 }
